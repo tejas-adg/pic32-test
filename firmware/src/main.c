ce38d435ce4f2f3764e4f4556a0b6c309c74c126
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2022-11-14 20:51:05 -0500

    this time error2

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -22,7 +22,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-#include <stddef.h>                     // Defines NULL
+include <stddef.h>                     // Defines NULL
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include "definitions.h"                // SYS function prototypes
